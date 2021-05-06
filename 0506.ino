610f18b6f8815144542b1bbf6bc0c6fa594f3981
Shang-ChiChen <83741986+Shang-ChiChen@users.noreply.github.com>
2021-05-06 11:31:50 +0800

    Add files via upload

---
+++ 0506.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(12,OUTPUT);
+  pinMode(2,INPUT_PULLUP);
+}
+
+void loop() {
+  int s =digitalRead(2);
+  if(s==HIGH){
+    digitalWrite(12,LOW);
+    Serial.println(s);
+    delay(100);
+  }else{
+    digitalWrite(12,HIGH);
+    Serial.println(s);
+    delay(100);
+    }
+  
+  // put your main code here, to run repeatedly:
+
+}
