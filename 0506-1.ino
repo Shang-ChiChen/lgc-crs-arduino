62db4c569055b6623c7d917f182005a1c704a56a
Shang-ChiChen <83741986+Shang-ChiChen@users.noreply.github.com>
2021-05-06 11:31:23 +0800

    Add files via upload

--- 0506-1.ino
+++ 0506-1.ino
@@ -1,14 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(12,OUTPUT);
   pinMode(2,INPUT_PULLUP);
 }
 
 void loop() {
-  if(digitalRead(2)==HIGH){
+  int s =digitalRead(2);
+  if(s==HIGH){
     digitalWrite(12,LOW);
+    Serial.println(s);
+    delay(1000);
   }else{
     digitalWrite(12,HIGH);
+    Serial.println(s);
+    delay(1000);
     }
   
   // put your main code here, to run repeatedly:
